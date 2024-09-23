d5189c20674429d7ef64ec3688c46493167ee519
IhorZolot <fedonn@ukr.net>
2024-09-23 15:44:43 +0300

    add sort

--- controllers/event-controller.js
+++ controllers/event-controller.js
@@ -2,10 +2,11 @@ import { ctrlWrapper } from '../decorators/index.js'
 import Event from '../models/Event.js'
 
 const getAllEvents = async (req, res) => {
-	const page = parseInt(req.query.page) || 1
-	const limit = 10
+	const {page=1, limit=6, sortBy = 'title', sortOrder = 'asc' } = req.query
 	const skip = (page - 1) * limit
-	const events = await Event.find({}, '-createdAt -updatedAt', { limit, skip })
+	const sortOptions = {};
+  sortOptions[sortBy] = sortOrder === 'asc' ? 1 : -1; 
+	const events = await Event.find({}, '-createdAt -updatedAt', { limit, skip }).sort(sortOptions);
 	const totalEvents = await Event.countDocuments()
 	const totalPages = Math.ceil(totalEvents / limit)
 	res.status(200).json({ events, totalPages })
@@ -14,7 +15,6 @@ const createEvent = async (req, res) => {
 	const event = await Event.create(req.body)
 	res.status(201).json(event)
 }
-
 
 export default {
 	getAllEvents: ctrlWrapper(getAllEvents),
